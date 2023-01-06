6e285a2f4c443f136e93e22f9f8f7e6c5df58493
Long_C <61483728+LongC-CN@users.noreply.github.com>
2023-01-06 16:08:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,8 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 # openwrt banner
 rm package/base-files/files/etc/banner
 echo " _   _ ___        ___ _____ _" >> package/base-files/files/etc/banner
-echo "| | | (_) \      / (_)  ___(_)      Compiled by Long_C" >> package/base-files/files/etc/banner
+echo "| | | (_) \      / (_)  ___(_)      Compiled by Long_C-CN
+" >> package/base-files/files/etc/banner
 echo "| |_| | |\ \ /\ / /| | |_  | | -------------------------------" >> package/base-files/files/etc/banner
 echo "|  _  | | \ V  V / | |  _| | | Lean's OpenWrt, %C" >> package/base-files/files/etc/banner
 echo "|_| |_|_|  \_/\_/  |_|_|   |_| -------------------------------" >> package/base-files/files/etc/banner
