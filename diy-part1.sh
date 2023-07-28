2788026e1b1cba3563a1dbad1cd0ed752fdb2e3d
龙C <longc.top@outlook.com>
2023-07-28 14:17:50 +0800

    add kenzok8/small-package

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git LongC https://github.com/LongC-CN/AutoBuild-Packages' >>feeds.conf.default
 #echo 'src-git temp https://github.com/Hyy2001X/AutoBuild-Packages' >>feeds.conf.default
-echo 'src-git NueXini https://github.com/NueXini/NueXini_Packages.git' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
